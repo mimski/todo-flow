70fda5f7662beaa80df68082f6a8c97bee480d90
mimski <maria.bg.1989@gmail.com>
2025-07-17 23:32:07 +0300

    added action section

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,3 +1,5 @@
+import { CheckCircle2, Plus, Trash2 } from "lucide-react";
+
 function TodoApp() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300 py-8 px-4">
@@ -14,7 +16,23 @@ function TodoApp() {
             <div className="text-2xl font-bold text-green-600"></div>
           </div>
         </div>
-        <div></div>
+        <div className="bg-white/90 backdrop-blur-sm rounded-2xl border border-gray-300 shadow-lg overflow-hidden">
+          <div className="p-6 border-b border-gray-300">
+            <div className="flex items-center justify-between mb-4">
+              <button className="flex items-center gap-3 bg-gray-800 hover:bg-gray-700 text-white px-4 py-2 rounded-lg transition-colors duration-200 font-medium cursor-pointer">
+                <Plus size={20} /> Add ToDo
+              </button>
+              <div className="flex items-center gap-2">
+                <button className="flex items-center gap-3  hover:bg-red-50 text-red-600 hover:text-red-700 px-3 py-2 rounded-lg transition-colors duration-200 text-sm cursor-pointer">
+                  <Trash2 size={16} /> Clear Completed
+                </button>
+                <button className="flex items-center gap-3  hover:bg-green-50 text-green-600 hover:text-green-700 px-3 py-2 rounded-lg transition-colors duration-200 text-sm cursor-pointer">
+                  <CheckCircle2 size={16} /> Mark All Completed
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
